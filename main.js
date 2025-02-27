4d89f240c2471e17ddf932c734af60323dc2a545
shabani005 <hasan.shabani2005@gmail.com>
2025-02-27 13:33:58 +0300

    smooth scroll and some changes

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 function scrollToSection(event, sectionId) {
     event.preventDefault();
     const target = document.getElementById(sectionId);
-    window.scrollTo({
-        top: target.offsetTop - 50, // Adjust for fixed navbar
-        behavior: 'smooth'
-    });
+
+    if (target) {
+        // Use element.scrollTo for better compatibility
+        window.scrollTo({
+            top: target.offsetTop - 50, // Adjust for fixed navbar
+            behavior: 'smooth'
+        });
+    } else {
+        console.error(`Section with id ${sectionId} not found`);
+    }
 }
